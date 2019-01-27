7c99477f4ffad3c6a2db65a1782e183b8920a227
Raj <raj.m.rao@gmail.com>
2019-01-26 22:53:00 -0700

    Create organizationsetting.js

---
+++ JavaScript/Samples/Xrm.WebApi/organizationsetting.js
@@ -0,0 +1,32 @@
+//https://docs.microsoft.com/en-us/dynamics365/customer-engagement/web-api/organization?view=dynamics-ce-odata-9
+//shows how to set enableunifiedinterfaceshellrefresh value as an example of the organization value.
+var entity = {};
+entity.enableunifiedinterfaceshellrefresh = true;
+
+Xrm.WebApi.online.retrieveMultipleRecords("organization", null).then(
+    function success(results) {
+        var organizationid = results.entities[0].organizationid;
+		//console.log(JSON.stringify(results.entities[0]));
+		console.log(results.entities[0].enableunifiedinterfaceshellrefresh);
+		if (results.entities[0].enableunifiedinterfaceshellrefresh == false)
+		{
+			console.log(`updating organization ${organizationid}`);
+			Xrm.WebApi.online.updateRecord("organization", organizationid, entity).then(
+				function success(result) {
+					console.log(`successfully updated organization ${organizationid}`);
+					var updatedEntityId = result.id;
+				},
+				function(error) {
+					Xrm.Utility.alertDialog(error.message);
+				}
+			);
+		}
+		else
+		{
+			console.log(`organization enableunifiedinterfaceshellrefresh is already set ${results.entities[0].enableunifiedinterfaceshellrefresh}`);
+		}
+    },
+    function(error) {
+        Xrm.Utility.alertDialog(error.message);
+    }
+);
